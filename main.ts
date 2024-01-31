7b9a8274d26b3fa7aae9b010f034fb35910a87cd
Dasa122 <153184061+Dasa122@users.noreply.github.com>
2024-01-31 15:28:23 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("fuck")
+})
+radio.onReceivedValue(function (name, value) {
+    if (name == "request") {
+        let PlayerIDs: number[] = []
+        serial.writeLine("" + value + " is trying to join...")
+        PlayerIDs.push(value)
+    } else if (name == "moveUp") {
+    	
+    } else {
+    	
+    }
+})
+radio.setGroup(144)
+datalogger.mirrorToSerial(true)
 basic.forever(function () {
-	
+    led.plotBrightness(0, 0, 255)
+    led.plotBrightness(0, 1, 50)
 })
